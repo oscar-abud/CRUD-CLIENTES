6ab99860e17dff13834b6759933832c9d552aa27
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-07-25 15:56:53 -0400

    Cambio en el nombre de los endpoints y agregando el middleware de cors

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
+const cors = require('cors')
 
 // Importando el controlador de clientes
 const Clients = require('./clients.controller.js')
@@ -11,14 +12,15 @@ const { User, isAuthenticated } = require('./user.controller.js')
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(cors())
 
 // Rutas de clientes
 app.get('/clients', isAuthenticated, Clients.list)
-app.post('/client', isAuthenticated, Clients.create)
-app.get('/client/:id', isAuthenticated, Clients.get)
-app.put('/client/:id', isAuthenticated, Clients.update)
-app.patch('/client/:id', isAuthenticated, Clients.update)
-app.delete('/client/:id', isAuthenticated, Clients.destroy)
+app.post('/clients', isAuthenticated, Clients.create)
+app.get('/clients/:id', isAuthenticated, Clients.get)
+app.put('/clients/:id', isAuthenticated, Clients.update)
+app.patch('/clients/:id', isAuthenticated, Clients.update)
+app.delete('/clients/:id', isAuthenticated, Clients.destroy)
 
 // Login y SignUp
 app.post('/login', User.login)
